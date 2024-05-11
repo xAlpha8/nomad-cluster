eb8db21ce396366e9dadd1607ae5a2178b1ce46c
Sahil Manocha <sahilmanocha1994@gmail.com>
2024-05-10 20:00:37 -0700

    changes

--- aws/main.tf
+++ aws/main.tf
@@ -109,28 +109,29 @@ resource "aws_security_group" "clients_ingress" {
   # These rules are applied only to the client nodes
 
   ingress {
-    from_port   = 0
+    from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
+    from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 0
-    to_port     = 5000
+    from_port   = 20000 
+    to_port     = 32000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 0
-    to_port     = 0 
+    from_port   = 20000
+    to_port     = 32000 
     protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
